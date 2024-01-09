60dc102e37f06332442e8d93cd4a38dc2bb3f60b
ClaudioScordari <scordari22@gmail.com>
2024-01-09 16:30:58 +0100

    Milestone 1 completato

--- js/script.js
+++ js/script.js
@@ -3,10 +3,37 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            myObj: {
-                text: '',
-                done: false
-            }
+
+            arrTodo: [
+                {
+                    text: 'Pane',
+                    done: false
+                },
+                {
+                    text: 'Pasta',
+                    done: true
+                },
+                {
+                    text: 'Mozzarella',
+                    done: true
+                },
+                {
+                    text: 'Acciughe',
+                    done: false
+                },
+                {
+                    text: 'Marmellata',
+                    done: false
+                },
+                {
+                    text: 'Nutella',
+                    done: true
+                },
+                {
+                    text: 'Vino',
+                    done: false
+                }
+            ]
         }
     }
 }).mount('#app')
